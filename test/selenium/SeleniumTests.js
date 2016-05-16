48bbdf7ac93f8722ead6908901df7b88ecd3f15a
lucas <lucasghizoni@gmail.com>
2016-05-16 01:51:18 -0300

    Removing interface test

--- test/selenium/SeleniumTests.js
+++ test/selenium/SeleniumTests.js
@@ -3,21 +3,21 @@ var chai = require('chai'),
     selenium = require('selenium-webdriver'),
     serverUri = 'http://localhost:3001';
     
+
 describe('Selenium Tests', function() {
     this.timeout(50000);
 
     beforeEach(function(done) {
-        this.driver = new selenium.Builder().
-            withCapabilities(selenium.Capabilities.chrome()).
-            build();
-
+        this.driver = new selenium.Builder()
+            .withCapabilities(selenium.Capabilities.chrome())
+            .build();
+            
         this.driver.get(serverUri).then(done);
     });
 
     afterEach(function(done) {
         this.driver.quit().then(done);
     });
-    
 
     it('As a costumer, i would like to have a card for news and sports products, and a basket for what products i choose', function(done) {
         var ids = ['news', 'sports', 'basket'];
@@ -31,7 +31,6 @@ describe('Selenium Tests', function() {
         }
         done();
     });
-    
     
     it('As a costumer, i would like to check a product on news card, and then he appears on basket', function(done) {
         var productName = "Sky News";
@@ -63,16 +62,14 @@ describe('Selenium Tests', function() {
             });
     });
     
-    
     it('Checkout button should appear disabled when there is no product selected', function(done) {
         var button = this.driver.findElement(selenium.By.xpath('//button[@id="checkout"]'));
         
         button.getAttribute('disabled').then(function(disabled){
             expect(disabled).to.equal('true');
             done();
-        });;
+        });
     });
-    
     
     it('Checkout button should appear enabled when costumer selects a product', function(done) {
         var productName = "Sky News";
@@ -89,34 +86,4 @@ describe('Selenium Tests', function() {
             done();
         });
     });
-    
-    
-    it('', function(done) {
-        var productNames = ["Sky News", "Sky Sports News"],
-            costumerID = '12332';
-        
-        for(key in productNames){
-          this.driver.findElement(selenium.By.xpath('//div[@id="news"]/label[text()="' + productNames[key] + '"]'))
-            .then(function(element){
-                element.click();
-            });
-        }
-        
-        var button = this.driver.findElement(selenium.By.xpath('//button[@id="checkout"]'));
-        button.click(); 
-        
-        this.driver.findElement(selenium.By.xpath("//h1[contains(text(), '" + costumerID +"')]"))
-            .then(function(element){
-                expect(element).to.not.equal('null');
-            });
-        
-        for (key in productNames){
-            this.driver.findElement(selenium.By.xpath("//li[text()='" + productNames[key] +"']"))
-            .then(function(element){
-                expect(element).to.not.equal('null');
-            });
-        }
-        done();
-    });
-    
 });
